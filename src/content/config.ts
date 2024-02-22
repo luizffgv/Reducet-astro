a0c2b43b973c9682016b4b4193ce4df912259709
Luiz Fernando F. G. Valle <luizfvalle@pm.me>
2024-02-21 23:12:44 -0300

    Add Zod descriptions

--- src/content/config.ts
+++ src/content/config.ts
@@ -3,10 +3,10 @@ import { z, defineCollection } from "astro:content";
 const postsCollection = defineCollection({
   type: "content",
   schema: z.object({
-    title: z.string(),
-    summary: z.string().max(200),
-    date: z.date(),
-    tags: z.array(z.string()),
+    title: z.string().describe("Title of the post."),
+    summary: z.string().max(200).describe("Short summary of the post."),
+    date: z.date().describe("Date the post was finished in."),
+    tags: z.array(z.string()).describe("Tags the post is marked with."),
   }),
 });
 
